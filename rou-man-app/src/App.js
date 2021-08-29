760fbf7ca13ac68602cd28b115b6a84378f890cc
Naiwrit <naiwrit.wrick@gmail.com>
2021-08-29 11:02:28 +0530

    Initial Commit

---
+++ rou-man-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Fragment } from 'react'
+import Navbar from './components/Navbar.js'
+import HeadBar from './components/HeadBar.js'
+function App() {
+  return (
+    <Fragment>
+        <Navbar/>
+        <HeadBar/>
+    </Fragment>
+
+  );
+
+}
+
+export default App;
