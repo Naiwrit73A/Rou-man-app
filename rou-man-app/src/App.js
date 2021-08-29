4e0710d346519894b793c46eb15bddb28132ae5f
Naiwrit <naiwrit.wrick@gmail.com>
2021-08-29 11:43:38 +0530

    Profile Card Added

--- rou-man-app/src/App.js
+++ rou-man-app/src/App.js
@@ -1,16 +1,26 @@
-import './App.css';
-import {Fragment } from 'react'
-import Navbar from './components/Navbar.js'
-import HeadBar from './components/HeadBar.js'
+import "./App.css";
+import { Fragment } from "react";
+import Navbar from "./components/Navbar.js";
+import ProfileCard from "./components/ProfileCard.js";
 function App() {
   return (
     <Fragment>
-        <Navbar/>
-        <HeadBar/>
+      <Navbar />
+      <div className="background">
+        <div className="bg-pattern-top"></div>
+        <div className="bg-pattern-bottom"></div>
+
+        <ProfileCard
+          name="Rita Correia"
+          age="32"
+          city="London"
+          followers="80K"
+          likes="803K"
+          photos="1.4K"
+        ></ProfileCard>
+      </div>
     </Fragment>
-
   );
-
 }
 
 export default App;
